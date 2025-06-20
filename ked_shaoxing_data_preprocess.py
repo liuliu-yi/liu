44a9cd0327fa80454eba1648e6ca093ebfa6648e
liuliu-yi <ly13619850960@163.com>
2025-06-20 10:53:49 +0800

    Add files via upload

--- ked_shaoxing_data_preprocess.py
+++ ked_shaoxing_data_preprocess.py
@@ -19,13 +19,7 @@ def handler_data():
     """"""
 
 
-    def str_to_list(x):
-        if isinstance(x, list):
-            return x
-        try:
-            return ast.literal_eval(x)
-        except:
-            return [x]
+ 
     #信号归一化
     def zscore_norm(ecg_signal):
             # (channels, timesteps)
@@ -35,12 +29,13 @@ def handler_data():
             return ecg_norm
 
     row_data_file = pd.read_csv('/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/new_diagnostics.csv')
-    print(row_data_file.shape)
-    print(row_data_file.head())
+    #print(row_data_file.shape)
+    #print(row_data_file.head())
 
     # 读取SNOMED映射表，建立映射字典
     map_df = pd.read_csv('/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/CSD/ConditionNames_SNOMED-CT.csv')
-    snomed_map = dict(zip(map_df['Snomed_CT'].astype(str), map_df['Full Name']))
+    map_df['Snomed_CT'] = map_df['Snomed_CT'].astype(str).str.strip().str.lstrip('0')
+    snomed_map = dict(zip(map_df['Snomed_CT'], map_df['Full Name']))
    
     # 先将所有的signal读取出来
     signal_data = []
@@ -71,10 +66,16 @@ def handler_data():
         row_data_file = row_data_file.drop(error_index).reset_index(drop=True)
 
     # 用Snomed_CT做多标签处理
-    # 多标签分割
-    row_data_file['Snomed_CT_list'] = row_data_file['Snomed_CT'].apply(
-        lambda x: [s.strip() for s in str(x).split(',') if s.strip()]
-    )
+    # 分割多标签字段
+    def split_snomed_codes(x):
+        return [s.strip().lstrip('0') for s in str(x).split(',') if s.strip()]
+    row_data_file['Snomed_CT_list'] = row_data_file['Snomed_CT'].apply(split_snomed_codes)
+    #映射
+    def snomed_to_names(code_list):
+        return [snomed_map.get(code, code) for code in code_list]
+
+    row_data_file['label'] = row_data_file['Snomed_CT_list'].apply(snomed_to_names)
+    print(row_data_file[['Snomed_CT', 'Snomed_CT_list', 'label']].head(10))
     # 可根据实际标签分隔符调整
 
     # 统计标签分布，过滤少见标签
@@ -112,47 +113,42 @@ def handler_data():
     lambda x: ast.literal_eval(x) if isinstance(x, str) and x.startswith('[') else x
     )
   
-    # Snomed_CT批量映射为多标签全称
-    def snomed_to_names(code_list):
-        return [snomed_map.get(str(code), str(code)) for code in code_list]
     
-    data_file['label']= data_file['Snomed_CT_list'].apply(snomed_to_names)
-
     #在划分之前保存一下 已经预处理好的完整表 方便后续统计标签
     data_file.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/processed_new_diagnostics.csv')
 
-    data_file=pd.read_csv('/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/processed_new_diagnostics.csv')
-    # 按8:1:1随机划分训练、验证、测试集
-    # 保证sig, label, report顺序一致
-    X_temp, y_test = train_test_split(
-        data_file, test_size=0.1, random_state=42, stratify=None
-    )
-    val_ratio = 0.1 / 0.9
-    y_train, y_val = train_test_split(
-        X_temp, test_size=val_ratio, random_state=42, stratify=None
-    )
+    # data_file=pd.read_csv('/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/processed_new_diagnostics.csv')
+    # # 按8:1:1随机划分训练、验证、测试集
+    # # 保证sig, label, report顺序一致
+    # X_temp, y_test = train_test_split(
+    #     data_file, test_size=0.1, random_state=42, stratify=None
+    # )
+    # val_ratio = 0.1 / 0.9
+    # y_train, y_val = train_test_split(
+    #     X_temp, test_size=val_ratio, random_state=42, stratify=None
+    # )
 
-    # 保存
-    #信号路径
-    root_dir = '/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/CSD/'
-    #拼接为完整路径
-    path_train = root_dir + y_train['filename'].astype(str)
-    path_val = root_dir + y_val['filename'].astype(str)
-    path_test = root_dir + y_test['filename'].astype(str)
+    # # 保存
+    # #信号路径
+    # root_dir = '/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/CSD/'
+    # #拼接为完整路径
+    # path_train = root_dir + y_train['filename'].astype(str)
+    # path_val = root_dir + y_val['filename'].astype(str)
+    # path_test = root_dir + y_test['filename'].astype(str)
 
-    path_train.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/ecg_train.csv',index=False)
-    path_val.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/ecg_val.csv',index=False)
-    path_test.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/ecg_test.csv',index=False)
+    # path_train.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/ecg_train.csv',index=False)
+    # path_val.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/ecg_val.csv',index=False)
+    # path_test.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/ecg_test.csv',index=False)
 
-    #标签
-    # 将每一行的list转为字符串再保存
-    y_train.astype(str).to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/label_train.csv',index=False) #默认保留索引与表头
-    y_val.astype(str).to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/label_val.csv',index=False)
-    y_test.astype(str).to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/label_test.csv',index=False)
-    #报告
-    report_train.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/report_train.csv',index=False)
-    report_val.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/report_val.csv',index=False)
-    report_test.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/report_test.csv',index=False)
+    # #标签
+    # # 将每一行的list转为字符串再保存
+    # y_train.astype(str).to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/label_train.csv',index=False) #默认保留索引与表头
+    # y_val.astype(str).to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/label_val.csv',index=False)
+    # y_test.astype(str).to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/label_test.csv',index=False)
+    # #报告
+    # report_train.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/report_train.csv',index=False)
+    # report_val.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/report_val.csv',index=False)
+    # report_test.to_csv(f'/data_C/sdb1/lyi/ked/ECGFM-KED-main/dataset/shaoxing/data/report_test.csv',index=False)
 
 
    
